a78594a529a4bbed0f50e9f3d124c8129ae1e579
ptmdmusique <ptmdmusic@gmail.com>
2021-02-06 11:10:56 -0800

    add firebase and deploy base app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import React from "react";
 import "./App.css";
 import logo from "./logo.svg";
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAu48AzSCNeIxovMLOrf9KIwbta2kh47MY",
+  authDomain: "cs465-565.firebaseapp.com",
+  projectId: "cs465-565",
+  storageBucket: "cs465-565.appspot.com",
+  messagingSenderId: "611141517345",
+  appId: "1:611141517345:web:2ac7d91ca19d480c276360",
+  measurementId: "G-NWFJR33125",
+};
+firebase.initializeApp(firebaseConfig);
 
 function App() {
   return (
