6ed0688a26c3fe37cf6eae9943406fb40b0b05d7
ptmdmusique <ptmdmusic@gmail.com>
2021-02-18 09:07:38 -0800

    clean up

--- src/App.tsx
+++ src/App.tsx
@@ -11,18 +11,7 @@ import {
   Switch,
 } from "react-router-dom";
 import "styles/App.css";
-import getClass from "./data/helpers/classHelper.js";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyAu48AzSCNeIxovMLOrf9KIwbta2kh47MY",
-  authDomain: "cs465-565.firebaseapp.com",
-  projectId: "cs465-565",
-  storageBucket: "cs465-565.appspot.com",
-  messagingSenderId: "611141517345",
-  appId: "1:611141517345:web:2ac7d91ca19d480c276360",
-  measurementId: "G-NWFJR33125",
-};
-firebase.initializeApp(firebaseConfig);
+import getClass from "data/helpers/classHelper.js";
 
 function App() {
   useEffect(() => {
