2d43f57615b0b0186d30e2a7fe893692a9af1776
Brandon McCoy <bdm@pdx.edu>
2021-02-28 15:40:02 -0800

    Add class and abilities api calls to store.

--- src/stores/api/api.store.tsx
+++ src/stores/api/api.store.tsx
@@ -1,15 +1,29 @@
+import axios from "axios";
 import getClass from "data/helpers/classHelper";
 import { alignmentList, DnDClass } from "data/model/dndModel";
-import { action, Action } from "easy-peasy";
+import { action, Action, thunk, Thunk } from "easy-peasy";
 
 export interface APIModel {
   classStat: Record<string, any> | null;
+  classes: Record<string, any> | null;
+  class: Record<string, any> | null;
+  abilities: Record<string, any> | null;
 
   generateDndClass: Action<APIModel, DnDClass>;
+  generateClasses: Thunk<APIModel>;
+  generateClass: Thunk<APIModel, string>;
+  generateAbilities: Thunk<APIModel>;
+
+  setClasses: Action<APIModel, any>;
+  setClass: Action<APIModel, any>;
+  setAbilities: Action<APIModel, any>;
 }
 
 export const api: APIModel = {
   classStat: null,
+  classes: null,
+  class: null,
+  abilities: null,
 
   generateDndClass: action((state, className) => {
     let classToGenerate = className;
@@ -24,4 +38,34 @@ export const api: APIModel = {
 
     state.classStat = getClass.generateDnDClass(classToGenerate);
   }),
+
+  generateClasses: thunk(async (actions) => {
+    const { data } = await axios.get(dndAPISrc + "classes/");
+    actions.setClasses(data.results);
+  }),
+
+  // payload: string name of class
+  generateClass: thunk(async (actions, payload) => {
+    const { data } = await axios.get(dndAPISrc + "classes/" + payload + '/');
+    actions.setClass(data);
+  }),
+
+  generateAbilities: thunk(async (actions) => {
+    const { data } = await axios.get(dndAPISrc + "ability-scores/");
+    actions.setAbilities(data);
+  }),
+
+  setClasses: action((state, dndClasses) => {
+    state.classes = dndClasses;
+  }),
+
+  setClass: action((state, dndClass) => {
+    state.class = dndClass;
+  }),
+
+  setAbilities: action((state, abilities) => {
+    state.abilities = abilities;
+  }),
 };
+
+const dndAPISrc = "https://www.dnd5eapi.co/api/";
