d3d3fb3dbbbeda1efb36a06d1e60fb1ddc8d512a
Brandon McCoy <bdm@pdx.edu>
2021-02-28 15:41:09 -0800

    Add dndClass to dndModel

--- src/data/model/dndModel.tsx
+++ src/data/model/dndModel.tsx
@@ -20,5 +20,20 @@ export const mapperClassList = [
 ] as const;
 export type DnDClass = typeof mapperClassList[number];
 
+export const dndClass = {
+  class_levels: "",
+  hit_die: 0,
+  index: "",
+  name: "",
+  proficiencies: [],
+  proficiency_choices: [],
+  saving_throws: [],
+  starting_equipment: [],
+  starting_equipment_options: [],
+  subclasses: [],
+  url: "",
+} as const;
+export type DnDClassStats = typeof dndClass;
+
 export const alignmentList = ["good", "evil", "neutral"];
 export type Alignment = typeof alignmentList[number];
