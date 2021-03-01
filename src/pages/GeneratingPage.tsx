67c2d76f39d7ded581144cc5978dc155210a8a83
Brandon McCoy <bdm@pdx.edu>
2021-02-28 16:24:04 -0800

    Revert GeneratingPage and dndModel

--- src/pages/GeneratingPage.tsx
+++ src/pages/GeneratingPage.tsx
@@ -1,5 +1,5 @@
-import { DnDClass, DnDClassStats, mapperClassList } from "data/model/dndModel";
-import React, { useEffect, Fragment } from "react";
+import { DnDClass, mapperClassList } from "data/model/dndModel";
+import React from "react";
 import { Button, Container, Form, ListGroup } from "react-bootstrap";
 import { useForm } from "react-hook-form";
 import { useStoreActions, useStoreState } from "stores";
@@ -10,13 +10,8 @@ interface FormField {
 export const GeneratingPage = () => {
   const { register, handleSubmit } = useForm<FormField>();
 
-  const { generateDndClass, generateClasses } = useStoreActions((actions) => actions.api);
+  const { generateDndClass } = useStoreActions((actions) => actions.api);
   const classStat = useStoreState((state) => state.api.classStat);
-  const classes = useStoreState((state) => state.api.classes);
-
-  useEffect(() => {
-    generateClasses();
-  }, [generateClasses]);
 
   const onSubmit = handleSubmit((data) => {
     generateDndClass(data.classToGenerate);
@@ -35,21 +30,11 @@ export const GeneratingPage = () => {
             name="classToGenerate"
             ref={register}
           >
-            {classes != null
-                  ? <Fragment>
-                    <option key={"dndClass" + 0} className="text-capitalize" value={'class'}>
-                      {"Random"}
-                    </option>
-                    {classes && classes.map((dndClass: any, index: number) => (
-                      <option key={"dndClass" + (index + 1).toString()} className="text-capitalize" value={dndClass.index}>
-                        {dndClass.name}
-                      </option>
-                    ))}
-                  </Fragment>
-                  : <option key={'loading'} className="text-capitalize" value={'loading'}>
-                    {"Loading..."}
-                  </option>
-                }
+            {mapperClassList.map((dndClass, index) => (
+              <option key={index} className="text-capitalize" value={dndClass}>
+                {dndClass === "class" ? "random" : dndClass}
+              </option>
+            ))}
           </Form.Control>
         </Form.Group>
 
