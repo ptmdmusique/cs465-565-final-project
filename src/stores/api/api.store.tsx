9ebda4f18340f97e57b40aacc1ea97ae6f8185bd
Brandon McCoy <bdm@pdx.edu>
2021-03-03 17:44:32 -0800

    Add traitMapper. Add race and equipment to the api store

--- src/stores/api/api.store.tsx
+++ src/stores/api/api.store.tsx
@@ -8,15 +8,24 @@ export interface APIModel {
   classes: Record<string, any> | null;
   class: Record<string, any> | null;
   abilities: Record<string, any> | null;
+  races: Record<string, any> | null;
+  race: Record<string, any> | null;
+  allEquipment: Record<string, any> | null;
 
   generateDndClass: Action<APIModel, DnDClass>;
   generateClasses: Thunk<APIModel>;
   generateClass: Thunk<APIModel, string>;
   generateAbilities: Thunk<APIModel>;
+  generateRaces: Thunk<APIModel>;
+  generateRace: Thunk<APIModel, string>;
+  generateAllEquipment: Thunk<APIModel>;
 
   setClasses: Action<APIModel, any>;
   setClass: Action<APIModel, any>;
   setAbilities: Action<APIModel, any>;
+  setRaces: Action<APIModel, any>;
+  setRace: Action<APIModel, any>;
+  setAllEquipment: Action<APIModel, any>;
 }
 
 export const api: APIModel = {
@@ -24,6 +33,9 @@ export const api: APIModel = {
   classes: null,
   class: null,
   abilities: null,
+  races: null,
+  race: null,
+  allEquipment: null,
 
   generateDndClass: action((state, className) => {
     let classToGenerate = className;
@@ -55,6 +67,21 @@ export const api: APIModel = {
     actions.setAbilities(data);
   }),
 
+  generateRaces: thunk(async (actions) => {
+    const { data } = await axios.get(dndAPISrc + "races/");
+    actions.setRaces(data);
+  }),
+
+  generateRace: thunk(async (actions, payload) => {
+    const { data } = await axios.get(dndAPISrc + "races/" + payload + "/");
+    actions.setRace(data);
+  }),
+
+  generateAllEquipment: thunk(async (actions) => {
+    const { data } = await axios.get(dndAPISrc + "equipment/");
+    actions.setAllEquipment(data);
+  }),
+
   setClasses: action((state, dndClasses) => {
     state.classes = dndClasses;
   }),
@@ -65,6 +92,18 @@ export const api: APIModel = {
 
   setAbilities: action((state, abilities) => {
     state.abilities = abilities;
+  }),
+
+  setRaces: action((state, races) => {
+    state.races = races;
+  }),
+
+  setRace: action((state, race) => {
+    state.race = race;
+  }),
+  
+  setAllEquipment: action((state, equipment) => {
+    state.allEquipment = equipment;
   }),
 };
 
